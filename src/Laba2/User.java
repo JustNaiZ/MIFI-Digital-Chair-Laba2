9e27833dddab7cc525ac7b401e2008628518ac61
JustNaiZ <tronov58@gmail.com>
2025-01-12 05:35:48 +0300

    User.java

--- src/Laba2/User.java
+++ src/Laba2/User.java
@@ -1,4 +1,40 @@
 package Laba2;
 
 public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String country;
+
+    public User(int id, String firstName, String lastName, int age, String country) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.country = country;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
